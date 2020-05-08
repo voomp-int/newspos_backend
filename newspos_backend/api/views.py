3d433f004c6edb86ebc586a5e38de75f9ee4114f
Subham Pramanik <subhampramanik@gmail.com>
2020-05-09 05:00:49 +0530

    feat: get all for all articles from DB

--- newspos_backend/api/views.py
+++ newspos_backend/api/views.py
@@ -1,8 +1,18 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from .serializers import ArticleSerializer
+from .models import Article
+from rest_framework import status
 
 
 @api_view(["GET"])
 def welcome(request):
     content = {"message": "Welcome to our shit!"}
     return JsonResponse(content)
+
+
+@api_view(["GET"])
+def get_articles(request):
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)
+    return JsonResponse({'articles': serializer.data}, status=status.HTTP_200_OK)
