5c0c39e56250885c03f53c6ec91058cd90b2c9e1
SubhamPramanik <subhampramanik@gmail.com>
2020-05-09 07:06:24 +0530

    [FEAT] CRUD Operations for Articles (#4)
    * fix: added id
    
    * feat: POST call to add articles
    
    * fix: added gitignore for Django
    
    * feat: PUT and DELETE operation

--- newspos_backend/api/views.py
+++ newspos_backend/api/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view
 from .serializers import ArticleSerializer
 from .models import Article
 from rest_framework import status
+import json
+from rest_framework.response import Response
 
 
 @api_view(["GET"])
@@ -16,3 +19,51 @@ def get_articles(request):
     articles = Article.objects.all()
     serializer = ArticleSerializer(articles, many=True)
     return JsonResponse({'articles': serializer.data}, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+def add_article(request):
+    payload = json.loads(request.body)
+    try:
+        article = Article.objects.create(
+            source=payload["source"],
+            author=payload["author"],
+            title=payload["title"],
+            description=payload["description"],
+            url=payload["url"],
+            urlToImage=payload["urlToImage"],
+            publishedAt=payload["publishedAt"]
+        )
+        serializer = ArticleSerializer(article)
+        return JsonResponse({'article': serializer.data}, status=status.HTTP_201_CREATED)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+    except Exception:
+        return JsonResponse({'error': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["PUT"])
+def alter_article(request, article_id):
+    payload = json.loads(request.body)
+    try:
+        article_item = Article.objects.filter(id=article_id)
+        article_item.update(**payload)
+        article = Article.objects.get(id=article_id)
+        serializer = ArticleSerializer(article)
+        return JsonResponse({'article': serializer.data}, status=status.HTTP_200_OK)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+    except Exception:
+        return JsonResponse({'error': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["DELETE"])
+def delete_article(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+    except Exception:
+        return JsonResponse({'error': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
